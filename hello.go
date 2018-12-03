df9713f50102d6a4ecbe5e1e7a4060a2d0ea2af8
monmee <jazzmonmee@gmail.com>
2018-12-04 00:04:02 +0900

    ch11: スライスを使ってみよう

--- hello.go
+++ hello.go
@@ -3,12 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// var a [5]int //a[0]-a[4]
-	// a[2] = 1
-	// a[3] = 2
-	// fmt.Println(a)
-	// b := [3]int{1, 3, 5}
-	b := [...]int{1, 3, 5}
-	fmt.Println(b)
-	fmt.Println(len(b))
+	a := [5]int{1, 3, 5, 7, 9}
+	s := a[2:4]
+	fmt.Println(a)
+	fmt.Println(s)
+	fmt.Println(len(s))
+	fmt.Println(cap(s))
+
 }
