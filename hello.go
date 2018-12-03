351bc8432afcce121d67acc2b610f2402844a9de
monmee <jazzmonmee@gmail.com>
2018-12-04 00:38:14 +0900

    ch14: ifで条件分岐してみよう

--- hello.go
+++ hello.go
@@ -3,15 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	// m := make(map[string]int)
-	// m["monmee"] = 200
-	// m["yuto"] = 100
-	m := map[string]int{"monmee": 200, "yuto": 100}
-	fmt.Println(m)
-	fmt.Println(len(m))
-	delete(m, "monmee")
-	fmt.Println(m)
-	val, isOk := m["yuto"]
-	fmt.Println(val)
-	fmt.Println(isOk)
+
+	// score := 43
+
+	if score := 63; score > 80 {
+		fmt.Println("Great!")
+	} else if score > 60 {
+		fmt.Println("Good.")
+	} else {
+		fmt.Println("So so.")
+	}
 }
