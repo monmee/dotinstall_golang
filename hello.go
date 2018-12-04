7da9cf0d2f535841643f50e70645f747d7524f7e
matsub <matsub.rk@gmail.com>
2018-12-04 19:00:59 +0900

    ch24: Webサーバーを作ってみよう

--- hello.go
+++ hello.go
@@ -5,26 +5,14 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
 )
 
-func task1(result chan string) {
-	time.Sleep(time.Second * 2)
-	result <- "task1 result"
-}
-
-func task2() {
-	fmt.Println("task2 finishied!")
-
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello %s!", r.URL.Path[1:])
 }
 
 func main() {
-	result := make(chan string)
-
-	go task1(result)
-	go task2()
-
-	fmt.Println(<-result)
-
-	time.Sleep(time.Second * 3)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
